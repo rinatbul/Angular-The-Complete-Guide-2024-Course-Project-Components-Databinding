390c6765a200af0a91c9b3649a5f3c430e5901eb
rinatbul <rinatbuldybayev@gmail.com>
2024-05-08 22:11:00 +0500

    90. Passing Data with Event and Property Binding (Combined)

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Recipe} from "../../recipe.model";
 
 @Component({
@@ -8,6 +8,11 @@ import {Recipe} from "../../recipe.model";
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe:Recipe
+  @Output() recipeSelected = new EventEmitter<void>()
+
+  onSelected(){
+    this.recipeSelected.emit()
+  }
   constructor() { }
 
   ngOnInit() {
